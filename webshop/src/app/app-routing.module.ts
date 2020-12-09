cab5af7518a6ade51544b92d0b9a4d3ac43d50ac
malev04 <martin.alev@gmail.com>
2020-12-09 11:22:33 +0200

    täiendav commit - 09.12.2020

---
+++ webshop/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { NotFoundComponent } from './global/not-found/not-found.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemViewComponent } from './item-view/item-view.component';
+
+const routes: Routes = [
+  { path: "", component: ItemListComponent },
+  { path: "view", component: ItemViewComponent },
+  { path: "cart", component: CartComponent },
+  { path: "**", component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ webshop/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './global/navbar/navbar.component';
+import { FooterComponent } from './global/footer/footer.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemViewComponent } from './item-view/item-view.component';
+import { CartComponent } from './cart/cart.component';
+import { NotFoundComponent } from './global/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ItemListComponent,
+    ItemViewComponent,
+    CartComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
