f6bc55aca77880c7146381bc8a0f7cdef440cf0f
malev04 <martin.alev@gmail.com>
2020-12-30 19:35:54 +0200

    arrays-people - 30.12.2020 - kiire fix vaja teha

--- kodused/arrays-people/src/app/valitud/valitud.service.ts
+++ kodused/arrays-people/src/app/valitud/valitud.service.ts
@@ -4,6 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ValitudService {
+  chosenPeople = [];
 
   constructor() { }
+
+  addHuman(human) {
+    this.chosenPeople = JSON.parse(localStorage.getItem("people")) || [];
+    this.chosenPeople.push(human);
+    localStorage.setItem("people", JSON.stringify(this.chosenPeople));
+  }
+
+  deleteHuman(index) {
+    this.chosenPeople = JSON.parse(localStorage.getItem("people")) || [];
+    this.chosenPeople.splice(index, 1);
+    localStorage.setItem("people", JSON.stringify(this.chosenPeople));
+  }
+
+  deleteAllPeople() {
+    this.chosenPeople = JSON.parse(localStorage.getItem("people")) || [];
+    this.chosenPeople = [];
+    localStorage.setItem("people", JSON.stringify(this.chosenPeople));
+  }
 }
