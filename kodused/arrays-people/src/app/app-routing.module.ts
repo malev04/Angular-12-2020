fe27154bddbed997dcae20335d4212baab79c13e
malev04 <martin.alev@gmail.com>
2021-01-20 18:12:48 +0200

    kodused array-people (uue isiku lisamine vormi abil)

--- kodused/arrays-people/src/app/app-routing.module.ts
+++ kodused/arrays-people/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PersonNewComponent } from './person-new/person-new.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,6 +7,7 @@ import { ValitudComponent } from './valitud/valitud.component';
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "chosen", component: ValitudComponent},
+  { path: "new", component: PersonNewComponent},
 ];
 
 @NgModule({
--- kodused/arrays-people/src/app/app.module.ts
+++ kodused/arrays-people/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ValitudComponent } from './valitud/valitud.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PersonNewComponent } from './person-new/person-new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ValitudComponent,
+    PersonNewComponent,
   ],
   imports: [
     BrowserModule,
